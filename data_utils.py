8f14e22e43a0a00b611129ae3574386527f87333
Cansutunc <cansutunc00@gmail.com>
2025-09-29 01:49:01 +0300

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -4,42 +4,34 @@ from PIL import Image
 import numpy as np
 import torch
 from torch.utils.data import Dataset, Subset
-import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
-# --- Original ImagesFolder Class (unchanged) ---
 class ImagesFolder(Dataset):
-    def __init__(self, root: str, size: Optional[Tuple[int,int]] = None):
+    def __init__(self, root: str, size: Optional[Tuple[int,int]] = None, subset_fraction=1.0):
         self.files=[]
         for ext in ('*.jpg','*.jpeg','*.png','*.bmp'):
             self.files+=glob.glob(os.path.join(root,ext))
-        self.files.sort(); self.size=size
-    def __len__(self): return len(self.files)
-    def __getitem__(self, idx):
-        path=self.files[idx]; img=Image.open(path).convert('RGB')
-        if self.size is not None: img=img.resize((self.size[1], self.size[0]), Image.BILINEAR)
-        x=torch.from_numpy((np.array(img).astype('float32')/255.).transpose(2,0,1))
-        return x, os.path.basename(path)
-
-# --- New UNupervised CocoStuff Class ---
-class CocoStuffUnsupervised(Dataset):
-    def __init__(self, root, annFile, size=(320, 320), subset_fraction=1.0):
-        self.coco = dset.CocoDetection(root, annFile)
-        self.size = size
+        
+        self.files.sort()
+        self.size=size
         self.transform = transforms.Compose([
             transforms.Resize(self.size),
             transforms.ToTensor(),
         ])
 
         if subset_fraction < 1.0:
-            num_samples = int(len(self.coco) * subset_fraction)
-            indices = np.random.choice(len(self.coco), num_samples, replace=False)
-            self.coco = Subset(self.coco, indices)
+            num_samples = int(len(self.files) * subset_fraction)
+            indices = np.random.choice(len(self.files), num_samples, replace=False)
+            self.files = [self.files[i] for i in indices]
 
-    def __len__(self):
-        return len(self.coco)
+    def __len__(self): 
+        return len(self.files)
 
-    def __getitem__(self, index):
-        # We only care about the image, so we ignore the target '_'
-        img, _ = self.coco[index] 
-        return self.transform(img), f"coco_img_{index}" # Return a dummy filename
+    def __getitem__(self, idx):
+        path=self.files[idx]
+        img=Image.open(path).convert('RGB')
+        
+        # Apply transformations
+        img_tensor = self.transform(img)
+        
+        return img_tensor, os.path.basename(path)
